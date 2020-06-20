Fix bug in AUC-ROC calculation, fix data paths

modules/dataset_retriever.py
@@ -10,8 +10,9 @@ def onehot(size, target):
 
 class DatasetRetriever(Dataset):
 
-    def __init__(self, kinds, image_names, labels, transforms=None):
+    def __init__(self, data_root_path, kinds, image_names, labels, transforms=None):
         super().__init__()
+        self.data_root_path = data_root_path
         self.kinds = kinds
         self.image_names = image_names
         self.labels = labels
@@ -19,7 +20,7 @@ class DatasetRetriever(Dataset):
 
     def __getitem__(self, index: int):
         kind, image_name, label = self.kinds[index], self.image_names[index], self.labels[index]
-        image = cv2.imread(f'{DATA_ROOT_PATH}/{kind}/{image_name}', cv2.IMREAD_COLOR)
+        image = cv2.imread(f'{self.data_root_path}/{kind}/{image_name}', cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
         image /= 255.0
         if self.transforms:

modules/rocauc_meter.py
@@ -23,9 +23,11 @@ def alaska_weighted_auc(y_true, y_valid):
         y_min = tpr_thresholds[idx]
         y_max = tpr_thresholds[idx + 1]
         mask = (y_min < tpr) & (tpr < y_max)
-        # pdb.set_trace()
-
-        x_padding = np.linspace(fpr[mask][-1], 1, 100)
+        
+        x_left = 0
+        if (np.any(mask) == True):
+            x_left = fpr[mask][-1]
+        x_padding = np.linspace(x_left, 1, 100)
 
         x = np.concatenate([fpr[mask], x_padding])
         y = np.concatenate([tpr[mask], [y_max] * len(x_padding)])