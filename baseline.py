2de3b87122ddc51502a534134c7adcd59bc3da2c
Chan Jun Shern <junshern@nutonomy.com>
2020-06-22 15:34:15 +0800

    Clean up baseline.py, add argparse options, support load from checkpoint

--- baseline.py
+++ baseline.py
@@ -8,9 +8,13 @@ import random
 import sklearn
 import time
 import torch
+import warnings
+warnings.filterwarnings("ignore")
 
 from albumentations.pytorch.transforms import ToTensorV2
+from catalyst.data.sampler import BalanceClassSampler
 from datetime import datetime
+from efficientnet_pytorch import EfficientNet
 from glob import glob
 from skimage import io
 from sklearn.model_selection import GroupKFold
@@ -23,14 +27,6 @@ from modules.dataset_submission_retriever import DatasetSubmissionRetriever
 from modules.fitter import Fitter
 from modules.train_global_config import TrainGlobalConfig
 
-# RUN PARAMS
-
-DATA_ROOT_PATH = './alaska2-image-steganalysis'
-
-# SETUP
-
-SEED = 42
-
 def seed_everything(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -39,10 +35,6 @@ def seed_everything(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
-
-seed_everything(SEED)
-
-# LOAD DATA
 
 def get_train_transforms():
     return A.Compose([
@@ -58,64 +50,12 @@ def get_valid_transforms():
             ToTensorV2(p=1.0),
         ], p=1.0)
 
-dataset = []
-for label, kind in enumerate(['Cover', 'JMiPOD', 'JUNIWARD', 'UERD']):
-    for path in glob(os.path.join(DATA_ROOT_PATH, 'Cover/*.jpg')):
-        dataset.append({
-            'kind': kind,
-            'image_name': path.split('/')[-1],
-            'label': label
-        })
-random.shuffle(dataset)
-print(len(dataset))
-dataset = pd.DataFrame(dataset)
-
-print(dataset.head())
-
-# Split into 5 set, validate on set 0, train on sets 1-4
-gkf = GroupKFold(n_splits=5)
-dataset.loc[:, 'fold'] = 0
-for fold_number, (train_index, val_index) in enumerate(gkf.split(X=dataset.index, y=dataset['label'], groups=dataset['image_name'])):
-    dataset.loc[dataset.iloc[val_index].index, 'fold'] = fold_number
-fold_number = 0
-
-train_dataset = DatasetRetriever(
-    DATA_ROOT_PATH,
-    kinds=dataset[dataset['fold'] != fold_number].kind.values,
-    image_names=dataset[dataset['fold'] != fold_number].image_name.values,
-    labels=dataset[dataset['fold'] != fold_number].label.values,
-    transforms=get_train_transforms(),
-)
-
-validation_dataset = DatasetRetriever(
-    DATA_ROOT_PATH,
-    kinds=dataset[dataset['fold'] == fold_number].kind.values,
-    image_names=dataset[dataset['fold'] == fold_number].image_name.values,
-    labels=dataset[dataset['fold'] == fold_number].label.values,
-    transforms=get_valid_transforms(),
-)
-
-image, target = train_dataset[0]
-numpy_image = image.permute(1,2,0).cpu().numpy()
-# fig, ax = plt.subplots(1, 1, figsize=(16, 8))    
-# ax.set_axis_off()
-# ax.imshow(numpy_image);
-
-import warnings
-warnings.filterwarnings("ignore")
-
-from efficientnet_pytorch import EfficientNet
-
 def get_net():
     net = EfficientNet.from_pretrained('efficientnet-b2')
     net._fc = nn.Linear(in_features=1408, out_features=4, bias=True)
     return net
 
-net = get_net().cuda()
-
-from catalyst.data.sampler import BalanceClassSampler
-
-def run_training():
+def run_training(checkpoint=None):
     device = torch.device('cuda:0')
 
     train_loader = torch.utils.data.DataLoader(
@@ -136,43 +76,90 @@ def run_training():
     )
 
     fitter = Fitter(model=net, device=device, config=TrainGlobalConfig)
-#     fitter.load(f'{fitter.base_dir}/last-checkpoint.bin')
+    if checkpoint is not None:
+        print("Resuming from checkpoint {}".format(checkpoint))
+        fitter.load(f'{fitter.base_dir}/last-checkpoint.bin')
     fitter.fit(train_loader, val_loader)
 
-# checkpoint = torch.load('../input/alaska2-public-baseline/best-checkpoint-033epoch.bin')
-# net.load_state_dict(checkpoint['model_state_dict'])
-run_training()
-net.eval() # Switch model to evaluation/inference mode
 
-# checkpoint.keys()
+if __name__ == "__main__":
 
-dataset = DatasetSubmissionRetriever(
-    image_names=np.array([path.split('/')[-1] for path in glob('../input/alaska2-image-steganalysis/Test/*.jpg')]),
-    transforms=get_valid_transforms(),
-)
+    # ARGS
+    parser = argparse.ArgumentParser(description="Training")
+    parser.add_argument("-d", "--datapath", type=str, metavar='', default="./alaska2-image-steganalysis", help="Path to root data dir. Default: %(default)s")
+    parser.add_argument("-c", "--checkpoint", type=str, metavar='', help="Resume from checkpoint.")
+    options = parser.parse_args()
 
-data_loader = DataLoader(
-    dataset,
-    batch_size=8,
-    shuffle=False,
-    num_workers=2,
-    drop_last=False,
-)
+    # SETUP
+    SEED = 42
+    seed_everything(SEED)
 
-result = {'Id': [], 'Label': []}
-for step, (image_names, images) in enumerate(data_loader):
-    print(step, end='\r')
-    
-    y_pred = net(images.cuda()) # 4-class classifcation: [5, 2, 4, 12]
-    one_hot_classification = nn.functional.softmax(y_pred, dim=1).data.cpu().numpy() # [0.2, 0.05, 0.015, 0.8]
-    prob_is_cover = one_hot_classification[:,0]
-    y_pred = 1 - prob_is_cover
-    
-    result['Id'].extend(image_names)
-    result['Label'].extend(y_pred)
+    # LOAD DATA
+    dataset = []
+    for label, kind in enumerate(['Cover', 'JMiPOD', 'JUNIWARD', 'UERD']):
+        for path in glob(os.path.join(options.datapath, 'Cover/*.jpg')):
+            dataset.append({
+                'kind': kind,
+                'image_name': path.split('/')[-1],
+                'label': label
+            })
+    random.shuffle(dataset)
+    dataset = pd.DataFrame(dataset)
+    gkf = GroupKFold(n_splits=5) # Split into 5 sets, validate on set 0, train on sets 1-4
+    dataset.loc[:, 'fold'] = 0
+    for fold_number, (train_index, val_index) in enumerate(gkf.split(X=dataset.index, y=dataset['label'], groups=dataset['image_name'])):
+        dataset.loc[dataset.iloc[val_index].index, 'fold'] = fold_number
+    fold_number = 0
+    train_dataset = DatasetRetriever(
+        options.datapath,
+        kinds=dataset[dataset['fold'] != fold_number].kind.values,
+        image_names=dataset[dataset['fold'] != fold_number].image_name.values,
+        labels=dataset[dataset['fold'] != fold_number].label.values,
+        transforms=get_train_transforms(),
+    )
+    validation_dataset = DatasetRetriever(
+        options.datapath,
+        kinds=dataset[dataset['fold'] == fold_number].kind.values,
+        image_names=dataset[dataset['fold'] == fold_number].image_name.values,
+        labels=dataset[dataset['fold'] == fold_number].label.values,
+        transforms=get_valid_transforms(),
+    )
 
-submission = pd.DataFrame(result)
-submission.to_csv('submission.csv', index=False)
-submission.head()
+    # TRAINING
+    net = get_net().cuda()
+    if options.checkpoint is not None:
+        # checkpoint = torch.load('../input/alaska2-public-baseline/best-checkpoint-033epoch.bin')
+        # net.load_state_dict(checkpoint['model_state_dict'])
+        run_training(checkpoint=options.checkpoint)
+    else:
+        run_training()
 
-# submission['Label'].hist(bins=100);
+    # TEST
+    net.eval() # Switch model to evaluation/inference mode
+    dataset = DatasetSubmissionRetriever(
+        image_names=np.array([path.split('/')[-1] for path in glob(os.path.join(options.datapath, 'Test/*.jpg'))]),
+        transforms=get_valid_transforms(),
+    )
+    data_loader = DataLoader(
+        dataset,
+        batch_size=8,
+        shuffle=False,
+        num_workers=2,
+        drop_last=False,
+    )
+    result = {'Id': [], 'Label': []}
+    for step, (image_names, images) in enumerate(data_loader):
+        print(step, end='\r')
+        
+        y_pred = net(images.cuda()) # 4-class classifcation: [5, 2, 4, 12]
+        one_hot_classification = nn.functional.softmax(y_pred, dim=1).data.cpu().numpy() # [0.2, 0.05, 0.015, 0.8]
+        prob_is_cover = one_hot_classification[:,0]
+        y_pred = 1 - prob_is_cover
+        
+        result['Id'].extend(image_names)
+        result['Label'].extend(y_pred)
+
+    submission = pd.DataFrame(result)
+    submission.to_csv('submission.csv', index=False)
+    submission.head()
+    # submission['Label'].hist(bins=100);
