56fff18f6b1225bc202ccb383269c1c08437843f
Chan Jun Shern <junshern@nutonomy.com>
2020-06-28 16:22:16 +0800

    Add gcloud install to script

--- scripts/setup_part1.sh
+++ scripts/setup_part1.sh
@@ -2,10 +2,19 @@
 
 # 1. Go to GCP console with your new account, Accept terms of services, activate $300 credit, enter personal and credit card details. 
 
-# 2. See which accounts are active from multiple google accounts, use these for setting up and navigating between several accounts and projects
-# gcloud auth list
+# 2. Install gcloud
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 
 # 3. Log in and authenticate Google SDK for your account when prompted
+# See which accounts are active from multiple google accounts, use these for setting up and navigating between several accounts and projects
+# gcloud auth list
 gcloud auth login $ACCOUNT_NAME
 
 # 4. Creating a new project and setting it as the current project
